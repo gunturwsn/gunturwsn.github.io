8f343fca67fe4892ad894a33cb44f7396cda401d
Guntur Wicaksono <43297078+gunturwsn@users.noreply.github.com>
2025-03-19 14:35:30 +0700

    Update projects.ts

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -15,7 +15,7 @@ export const projects: Project[] = [
     title: "Note Me",
     description: "Simple note-taking app built with Nuxt.js, MySql, Prisma.",
     tech: ["Vue.js", "Nuxt.js", "MySQL", "Prisma"],
-    image: "/images/noteme.png",
+    image: `/images/noteme.png`,
     github: "https://github.com/gunturwsn/my-portfolio",
   },
 ];
